b66852f650d0a4ce849be19ae5bddc283afa5333
Gustav <gustav.selander2007@gmail.com>
2022-12-28 23:44:44 +0100

    uppdaterade template

--- templates/command.js
+++ templates/command.js
@@ -2,9 +2,11 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('commandName')
-        .setDescription('Beskrivningen på kommandot.'),
-        async execute(interaction) {
-
-        }
+        .setName('Namn')
+        .setDescription('Description'),
+    async execute(interaction) {
+        interaction.reply({
+            content: "Vad den ska svara"
+        })
+    }
 }
