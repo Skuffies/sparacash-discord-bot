592dfd3df2f9a853e57c17a70b67efc70a5ffe02
Gustav <gustav.selander2007@gmail.com>
2023-01-02 11:11:28 +0100

    warrant kommando finns

---
+++ commands/stock-market/warrant.js
@@ -0,0 +1,57 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { EmbedBuilder } = require('discord.js');
+
+const axios = require('axios');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('warrant')
+        .setDescription('Hitta information om en warrant.')
+        .addStringOption(option =>
+            option
+            .setName('namn')
+            .setDescription('Namnet på warranten du vill hitta information om.')
+            .setRequired(true)
+            ),
+        async execute(interaction) {
+            const warrant = interaction.options.getString('namn');
+
+            axios.get(`https://www.avanza.se/_mobile/market/search?query=${warrant}`).then(res => {
+
+                let warrantName = res['data']['hits'][0]['topHits'][0]['name'].toString();
+                let warrantPrice = res['data']['hits'][0]['topHits'][0]['lastPrice'].toString();
+                let warrantCurrency = res['data']['hits'][0]['topHits'][0]['currency'].toString();
+                let warrantPercent = res['data']['hits'][0]['topHits'][0]['changePercent'].toString();
+                let warrantCountry = res['data']['hits'][0]['topHits'][0]['flagCode'].toString();
+                let warrantId = res['data']['hits'][0]['topHits'][0]['id'].toString();
+
+                if (!warrantPercent.includes('-')) {
+                    warrantPercent = `+${warrantPercent}`
+                }
+
+                const infoEmbed = new EmbedBuilder()
+                .setColor('#2fd8eb')
+                .setTitle('Warrant information')
+                .setFooter({
+                    text: '*Upp till 15 min fördröjning*'
+                })
+                .addFields(
+                    {name: 'Namn', value: warrantName},
+                    {name: 'Procentuell förändring', value: `${warrantPercent}%`},
+                    {name: 'Pris', value: `${warrantPrice} ${warrantCurrency}`},
+                    {name: 'Landskod', value: warrantCountry},
+                    {name: 'Länk', value: `[${warrantName} på Avanza](https://click.adrecord.com/?c=24113&p=836&epi=discord&url=https://www.avanza.se/borshandlade-produkter/warranter-torg/om-warranten.html/${warrantId}/${warrantName.replaceAll(' ', '-')})`},
+                )
+
+                interaction.reply({
+                    embeds: [infoEmbed]
+                });
+
+            }).catch(error => {
+                interaction.reply({
+                    content: 'Kunde inte hitta warranten.'
+                });
+
+            });
+    }
+}
